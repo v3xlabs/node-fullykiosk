589f8fb23ce55539999722cbc1745649d3a5dc6e
Luc <luc@lucemans.nl>
2023-02-02 18:48:11 +0000

    Introduce useHostname (#1)

--- test/App.tsx
+++ test/App.tsx
@@ -1,14 +1,28 @@
 import { FC } from 'react';
 
-import { useCurrentLocale } from '../src/index';
+import {
+    useCurrentLocale,
+    useHostname,
+    useHostname6,
+    useIpv4Address,
+    useIpv6Address,
+} from '../src/index';
 
 export const App: FC = () => {
     const currentLocale = useCurrentLocale();
+    const ipv4 = useIpv4Address();
+    const ipv6 = useIpv6Address();
+    const hostname = useHostname();
+    const hostname6 = useHostname6();
 
     return (
         <div>
             <h1>Test Page</h1>
             <p>Current locale: {currentLocale}</p>
+            <p>Current ipv4: {ipv4}</p>
+            <p>Current ipv6: {ipv6}</p>
+            <p>Current hostname: {hostname}</p>
+            <p>Current hostname6: {hostname6}</p>
         </div>
     );
 };
