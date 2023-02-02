4ae802e06067e58e10bf920d9b0a2512a037a189
Luc <luc@lucemans.nl>
2023-02-02 18:51:21 +0000

    Introduce useMacAddress (#2)
    * Introduce useHostname
    
    * Introduce useMacAddress

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export * from './hooks/useCurrentLocale';
 export * from './hooks/useHostname';
 export * from './hooks/useIpv4Address';
 export * from './hooks/useIpv6Address';
+export * from './hooks/useMacAddress';
