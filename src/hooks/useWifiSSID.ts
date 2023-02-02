cba7fc17cdda19605af46826fe7e7034c8813aca
Lucemans <luc@lucemans.nl>
2023-02-02 18:44:16 +0000

    Introduce useWifiBSSID

--- src/hooks/useWifiSSID.ts
+++ src/hooks/useWifiSSID.ts
@@ -3,3 +3,7 @@ import { wrapBasicFunction } from '../utils/basic_hook';
 // Gets the wifi SSID
 // Returns undefined if fully not available
 export const useWifiSSID = wrapBasicFunction(() => fully.getWifiSsid());
+
+// Gets the wifi BSSID
+// Returns undefined if fully not available
+export const useWifiBSSID = wrapBasicFunction(() => fully.getWifiBssid());
