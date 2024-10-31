f74d5883052f0b4517ea0e1d25ac26fe99e48898
Luc <luc@lucemans.nl>
2024-10-31 19:14:55 +0000

    Update useNFC

--- src/hooks/useNFC.ts
+++ src/hooks/useNFC.ts
@@ -1,25 +1,31 @@
 import { useCallback, useEffect, useState } from 'react';
 import { is_fully } from '../utils/is_fully';
 
-export const useNFC = (config?:
-    { onNdef: (serial: string, message: string, data: string) => void, onNfcTagDiscovered: (serial: string, type: string, message: string, data: string) => void, onNfcTagRemoved: (serial: string) => void }) => {
-    if (!is_fully()) {
-        return;
+const preflight = () => {
+    if (!is_fully) {
+        alert('Fully not available');
+        throw new Error('Fully not available');
     }
+};
 
+export const useNFC = (config?:
+    { onNdef?: (serial: string, message: string, data: string) => void, onNfcTagDiscovered?: (serial: string, type: string, message: string, data: string) => void, onNfcTagRemoved?: (serial: string) => void }) => {
     const [isScanning, setIsScanning] = useState(false);
 
     const start = useCallback(() => {
+        preflight();
         fully.nfcScanStart();
         setIsScanning(true);
     }, []);
 
     const stop = useCallback(() => {
+        preflight();
         fully.nfcScanStop();
         setIsScanning(false);
     }, []);
 
     useEffect(() => {
+        preflight();
         // @ts-ignore
         window.onNdefDiscovered = (serial: string, message: string, data: string) => {
             if (config?.onNdef) {
